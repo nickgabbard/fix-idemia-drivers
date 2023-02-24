45b297f37a209f4413316dab0f06d130607bae51
nickgabbard <nickgabbard@gmail.com>
2023-02-24 09:29:40 -0500

    Add files via upload

---
+++ DeleteOldIdemiaDriver.ps1
@@ -0,0 +1,4 @@
+﻿ $DriverTable = Get-WindowsDriver –Online| select Driver, ClassName, BootCritical, ProviderName, Date, Version, OriginalFileName| Where {($_.ClassName -Match "SmartCard") -and ($_.ProviderName -Match "IDEMIA") -and ($_.Version -Match "1.0.0.902")} 
+ $DeleteCommand =  "pnputil.exe /delete-driver $($DriverTable.Driver) /force"
+ Write-Output $DeleteCommand
+ Start-Process PNPUtil -ArgumentList "/delete-driver",$($DriverTable.Driver),"/force"
