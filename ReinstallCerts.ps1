f35d7ede760edf02626eee8133980f086babbf57
Haverghast <126290546+Haverghast@users.noreply.github.com>
2023-02-24 16:23:05 -0500

    Create ReinstallCerts.ps1
    Remove and reinstall certificates

---
+++ ReinstallCerts.ps1
@@ -0,0 +1,11 @@
+$ConfirmPreference = 'None'
+
+#Thumbprint of certificate to remove
+$Thumb = "abcdef444444857694df5e45b68851868"
+
+#Loop through all the certs stores looking for a matching thumbprint and remove if found
+Get-ChildItem Cert:/ -Recurse | Where-Object {$_.thumbprint -Contains "$Thumb"} | Remove-Item -Force
+
+
+Import-Certificate -FilePath "PATH" -CertStoreLocation Cert:\CurrentUser
+Import-Certificate -FilePath "PATH" -CertStoreLocation Cert:\LocalMachine
