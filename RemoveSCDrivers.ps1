3f22f5cad94123a4e62247baa96efb62ca3e604b
Haverghast <126290546+Haverghast@users.noreply.github.com>
2023-02-24 16:07:48 -0500

    Create RemoveSCDrivers.ps1
    Remove all drivers with the SmartCard class.

---
+++ RemoveSCDrivers.ps1
@@ -0,0 +1,8 @@
+ $DriverTable = Get-WindowsDriver –Online| select Driver, ClassName | Where {($_.ClassName -Match "SmartCard")} | Select-Object Driver
+ $RMDriver = $DriverTable.Driver
+
+ ForEach($SCDriver in $RMDriver){
+    $DeleteCommand =  "pnputil.exe /delete-driver $($SCDriver) /force"
+    Write-Output $DeleteCommand
+    Start-Process PNPUtil -ArgumentList "/delete-driver",$($SCDriver),"/force" WhatIf
+}
